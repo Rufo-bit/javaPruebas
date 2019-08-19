1ef3176a13ce9cf260618441d12507f4b2f78eba
Rufo-bit <antonio.muram0@gmail.com>
2019-08-19 01:34:52 -0500

    Se añadio un txt y un java

---
+++ primerPrograma/src/primerprograma/bucles_uno.java
@@ -0,0 +1,25 @@
+
+package primerprograma;
+
+import javax.swing.*;
+public class bucles_uno 
+{
+    public static void main(String[]cargs)
+    {
+        
+        String clave="Rufino";
+        String contra="";
+        
+        
+        while(clave.equals(contra)==false)//Si la clave no es igual a contra entonces
+        {
+            contra = JOptionPane.showInputDialog("Introduce la contraseña por favor: ");
+            
+            if(clave.equals(contra)==false)
+            {
+                System.out.println("Contraseña incorrecta");
+            }
+        }
+        System.out.println("Contraseña correcta, accso permitido");
+    }
+}
