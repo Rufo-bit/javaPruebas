052efa0e2dfd9a39c9fe1b3d3b53c522ca5b1b5c
Rufo-bit <antonio.muram0@gmail.com>
2019-09-12 01:07:22 -0500

    Se creo un nuevo paquete, su clase y un txt

---
+++ primerPrograma/src/poo/Coche.java
@@ -0,0 +1,25 @@
+/*
+Empiezo a programar en Java
+Concepto de clase, objeto y método
+Antonio
+*/
+package poo;
+public class Coche
+{
+    int ruedas;
+    int largo;
+    int ancho;
+    int motor;
+    int peso;
+    
+    //Se declara un método constructor
+    
+    public Coche()
+    {
+        ruedas=4;
+        largo=2000;
+        ancho=300;
+        motor=1600;
+        peso=500;
+    }
+}
---
+++ primerPrograma/src/poo/Uso_coche.java
@@ -0,0 +1,18 @@
+
+package poo;
+
+
+public class Uso_coche
+{
+    public static void main(String[] args)
+    {
+        Coche Renault = new Coche();//Aqui se instancia una clase, ejemplar de una clase
+                                    //Renault es el ejemplar de la clase Coche
+                                    
+        
+        System.out.println("Este coche tiene" + Renault.ruedas + "ruedas");
+        {
+            
+        }
+    }
+}
