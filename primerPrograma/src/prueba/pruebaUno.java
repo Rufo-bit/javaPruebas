96ee9e9a0311a77da5da736ba93433e121428889
rufo-bit <antonio.muram0@gmail.com>
2019-09-26 01:05:23 -0500

    Prueba para verificar si toda va en orden

---
+++ primerPrograma/src/prueba/pruebaUno.java
@@ -0,0 +1,16 @@
+/*
+Ejercicio de prueba
+ */
+package prueba;
+import javax.swing.*;
+public class pruebaUno 
+{
+public static void main(String[] args)
+{
+    String nombre = JOptionPane.showInputDialog("Ingresa tu nombre");
+
+    System.out.println("Hola" + nombre + "hace tiempo sin verte");
+    
+    
+}
+}
