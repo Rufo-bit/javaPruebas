b9e0f0bc36dc4e216ac141253028e9c0b1767c26
Rufo-bit <antonio.muram0@gmail.com>
2019-10-09 00:54:32 -0500

    se edito un java y un txt

--- primerPrograma/src/poo/empleado.java
+++ primerPrograma/src/poo/empleado.java
@@ -93,11 +93,12 @@ public class empleado
 {
     public static void main(String[] args)
     {
-        clase_empleado[] misEmpleados = new clase_empleado[3];
+        clase_empleado[] misEmpleados = new clase_empleado[4];
         
         misEmpleados[0] = new clase_empleado("Antonio Muñoz", 85000, 2005, 12, 3);
         misEmpleados[1] = new clase_empleado("Fabian Muñoz", 95000, 2009, 6, 2);
         misEmpleados[2] = new clase_empleado("Victor Muñoz", 100000, 20015, 11, 5);
+        misEmpleados[3] = new clase_empleado(" José Antonio"); //Llamara al segundo contructor por tener un argumento
         
         /*for(int i=0; i<3; i++)
         {
@@ -110,7 +111,7 @@ public class empleado
         }
         
         
-        for (int i=0;i<3;i++)
+        for (int i=0;i<4;i++)
         {
             System.out.println("Nombre: " + misEmpleados[i].dameNombre() + " Sueldo: " + 
                     misEmpleados[i].dameSueldo() + " Fecha alta: " + misEmpleados[i].dameAlta());
@@ -121,7 +122,7 @@ public class empleado
 class clase_empleado
 {
     //método contructor que recibira argumentos
-    public clase_empleado(String nom, double sue, int agno, int mes, int dia)
+    public clase_empleado(String nom, double sue, int agno, int mes, int dia) //un constructor no lleva void y no devuelve datos
             {
                 nombre = nom;
                 sueldo = sue;
@@ -133,6 +134,22 @@ class clase_empleado
                 altaContrato=calendario.getTime(); //El get llama los metodos de la clase GregorianCalendar
             
             }
+    
+    
+     //Sobrecarga de constructores
+    //Es cuando quieres utilizar varios constructores dentro de una misma clase
+    //deben tener direntes tipos de argumentos para no generar confusion en el compilador
+    
+    
+    public clase_empleado(String nom) //Segundo Constructor
+    {
+        this(nom, 30000, 2000, 01, 01);//El this dentro de un constructor, llama al otro constructor de la otra que coincida 
+                                        //con sus parametros correspondientes
+        
+    
+    }
+    
+    
     
     public String dameNombre()//GETTER
     {
