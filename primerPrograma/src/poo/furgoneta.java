b87c8d1d4cacbe1639a7da36f79d21c8dd2260d9
Rufo-bit <antonio.muram0@gmail.com>
2019-10-10 01:37:38 -0500

    Se añadio un txt y un .java

---
+++ primerPrograma/src/poo/furgoneta.java
@@ -0,0 +1,20 @@
+
+package poo;
+
+//Antonio Muñoz
+//Esta clase contendras los temas de herencia
+
+public class furgoneta /*subclase*/extends Coche //superclase
+{
+    private int capacidad_carga;
+    private int lugares_extra;
+
+    
+    public furgoneta(int lugares_extra, int capacidad_carga)
+    {
+        super();//la instrucción super llama al construcotr de la clase padre
+        this.capacidad_carga=capacidad_carga;
+        
+        this.lugares_extra=lugares_extra;
+    }
+}
