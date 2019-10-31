79b06ed58eff0bfea7bf79bab6e67f8333b4c47c
Rufo-bit <antonio.muram0@gmail.com>
2019-10-31 01:06:42 -0600

    txt

--- primerPrograma/src/ProyectoFinal/Empleado.java
+++ primerPrograma/src/ProyectoFinal/Empleado.java
@@ -3,12 +3,25 @@
 Se creara un sistema de nomina de Empleados
  */
 package ProyectoFinal;
+
+import java.util.*;
+
 class Empleado
 {
     //Declarar variables
     
+    //cod_empleado
+    private String nombre;
+    private int edad;
+    private char RFC;
+    private double sueldo_base;
+    private double comision;
+    private int dias_laborados;
+    private char turno;
+    private float horas_laboradas;
+    private Date alta_contrato;
     
-    public Empleado()
+    public Empleado(String nom, )
     {
         
     }
