3f925d920fae1da7e7e3d29103218ed58aa3c6b8
Rufo-bit <antonio.muram0@gmail.com>
2019-11-04 01:46:17 -0600

    Cambios en proyecto

--- primerPrograma/src/ProyectoFinal/Empleado.java
+++ primerPrograma/src/ProyectoFinal/Empleado.java
@@ -21,9 +21,42 @@ class Empleado
     private float horas_laboradas;
     private Date alta_contrato;
     
-    public Empleado(String nom, )
+    public Empleado(String nom, int edad, char RFC, double sueldo_base, int dias_laborados, char turno, float horas_laboradas, Date alta_contrato )
     {
-        
+     nombre = nom;
+     this.edad = edad;
+     this.RFC = RFC;
+     this.sueldo_base = sueldo_base;
+     this.dias_laborados = dias_laborados;
+     this.turno = turno;
+     this.horas_laboradas = horas_laboradas;
+     this.alta_contrato = alta_contrato;
+     
     }
+    
+    //GETTER Y SETTER
+    
+    //La regla del negocio es que por cada hora extra trabajada se acumulara 150 pesos más al trabajador,
+    //Las horas de trabajo seran 7 horas por dias
+    //Si trabajan un día extra se pagara 1200
+    
+    
+    public void Setnombre (String escribe_nombre)
+    {
+        this.nombre = escribe_nombre;
+    }
+    
+    public void
+            {
+                
+            }
+            
+            
+    public void SetSueldoBase (int escribe_sueldo)
+    {
+        this.sueldo_base = escribe_sueldo;
+    }
+    
+    
 }
 
